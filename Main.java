9952b55885c5b492e038c876c1c66345930fd18c
SpencerBalmer <cougrexs@gmail.com>
2024-10-08 13:20:30 -0500

    Submit

--- Main.java
+++ Main.java
@@ -1,8 +1,52 @@
+
+import java.util.Scanner;
 public class Main {
-    public static void main(String []args) {
+
+
+public static void main(String []args) {
+    Scanner input = new Scanner(System.in);
     Methods M = new Methods();
     int DH = M.Deal();
-    System.out.println(DH);
+    System.out.println("Dealer Hand " + DH);
+    int PH = M.Deal() + M.Deal();
+    System.out.println("Player Hand " + PH);
+    if (PH == 22){
+        PH = 21;
+    }
+    boolean PlayerBust = false;
+    while (PH <= 21){
+    System.out.println("Hit or stand");
+    
+    String HitStand = input.next();
+    if (HitStand.equals("hit")){
+        PH = PH + M.Deal();
+        System.out.println("New player hand " + PH);
+    
+        if (PH > 21){
+        System.out.println("Bust");
+        PlayerBust = true;
+    }
+    }else{
+        break;
+    }    
+    }
+    while (DH < 17 && PlayerBust == false && PH !=21){
+        DH = DH + M.Deal();
+        System.out.println("dealer new hand " + DH);
+    }
+        Methods.Level Result = M.Winner(DH, PH);
+    if (Result.equals(Methods.Level.PlayerWin)){
+        System.out.println("player wins");
+    }
+    else if (Result.equals(Methods.Level.DealerWin)){
+        System.out.println("dealer wins");
+    }
+    else{
+        System.out.println("Tie");
+    }
+}
+
+
 }
     
-    }  
+    
--- Methods.java
+++ Methods.java
@@ -1,5 +1,32 @@
 public class Methods {
-    public Integer Deal(){
+  enum Level {
+    DealerWin,
+    PlayerWin,
+    Tie
+  }  
+  public Integer Deal(){
       return (int)(Math.random()*11)+1;
     }
-}
+    
+    
+    
+    public Level Winner(int DealerHand, int PlayerHand){
+      if (PlayerHand >= 22){
+        return Level.DealerWin;
+      }
+      else if (DealerHand > PlayerHand && DealerHand <= 21){
+        return Level.DealerWin;
+      }
+      else if (DealerHand >= 22){
+        return Level.PlayerWin;
+      }
+      else if (PlayerHand > DealerHand && PlayerHand <= 21){
+        return Level.PlayerWin;
+      }
+      else{
+        return Level.Tie;
+      }
+    }
+
+  }
+
